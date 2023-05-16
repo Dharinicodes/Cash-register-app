4ba96c29ecec18f69b2211980bf4b3aa33787ddc
Dharinicodes <126567741+Dharinicodes@users.noreply.github.com>
2023-05-16 17:49:10 +0530

    added js doc

---
+++ index.js
@@ -0,0 +1,38 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+  hideMessage();
+  if (billAmount.value > 0) {
+    if (cashGiven.value >= billAmount.value) {
+      const amountToBeReturned = cashGiven.value - billAmount.value;
+      calculateChange(amountToBeReturned);
+    } else {
+      showMessage("Do you wanna wash plates?");
+    }
+  } else {
+    showMessage("Invalid Bill Amount");
+  }
+});
+
+function calculateChange(amountToBeReturned) {
+  for (let i = 0; i < availableNotes.length; i++) {
+    const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+    amountToBeReturned = amountToBeReturned % availableNotes[i];
+    noOfNotes[i].innerText = numberOfNotes;
+  }
+}
+
+function hideMessage() {
+  message.style.display = "none"; //to hide the messages from not displaying when it is not required
+}
+
+function showMessage(msg) {
+  message.style.display = "block"; // to show the text below only if bill amount is lesser than 0
+  message.innerText = msg;
+}
